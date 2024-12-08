9e5f2756ed5acdd5cd8640f22bfa6150c724c15b
Juan Cruz <juancasserab@gmail.com>
2024-12-08 17:23:43 -0300

    Cambio esto porque no me dejaba compilar....

--- Práctica/3/src/grpc-chat-grupal/src/main/java/pdytr/ejercicio3/ChatServer.java
+++ Práctica/3/src/grpc-chat-grupal/src/main/java/pdytr/ejercicio3/ChatServer.java
@@ -1,18 +1,20 @@
-    package pdytr.ejercicio3;
+package pdytr.ejercicio3;
 
-    import io.grpc.Server;
-    import io.grpc.ServerBuilder;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
 
-    import java.io.IOException;
+import java.io.IOException;
 
-    public class ChatServer {
+public class ChatServer {
 
-        public static void main(String[] args) throws IOException, InterruptedException {
-            Server server = ServerBuilder.forPort(50051)
-                    .addService(new ChatServiceImpl())
-                    .build();
-            System.out.println("\n\n\nChat Server iniciado en el puerto 50051.");
-            server.start();
-            server.awaitTermination();
-        }
+    public static void main(String[] args) throws IOException, InterruptedException {
+        ChatServiceImpl chatService = new ChatServiceImpl();
+
+        Server server = ServerBuilder.forPort(50051)
+                .addService(chatService)
+                .build();
+        System.out.println("\n\n\nChat Server iniciado en el puerto 50051.");
+        server.start();
+        server.awaitTermination();
     }
+}
